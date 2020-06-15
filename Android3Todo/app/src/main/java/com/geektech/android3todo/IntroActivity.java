f3a684186d3c846cd88e596477ec801553ccbcaa
imgeek007 <natlusrun94@gmail.com>
2020-06-15 15:55:47 +0300

    IntroActivity with fragments

--- Android3Todo/app/src/main/java/com/geektech/android3todo/IntroActivity.java
+++ Android3Todo/app/src/main/java/com/geektech/android3todo/IntroActivity.java
@@ -1,14 +1,40 @@
 package com.geektech.android3todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.geektech.android3todo.fragments.Fragment1;
+import com.geektech.android3todo.fragments.Fragment2;
+import com.geektech.android3todo.fragments.Fragment3;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new Fragment1());
+        fragments.add(new Fragment2());
+        fragments.add(new Fragment3());
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
+
+        viewPager.setAdapter(viewPagerAdapter);
     }
+
+
 }
