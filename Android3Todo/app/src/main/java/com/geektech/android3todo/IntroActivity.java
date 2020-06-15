f7f0a3a38adb7b17a8ec6d86f7d30ca4155eb5d8
imgeek007 <natlusrun94@gmail.com>
2020-06-15 14:43:41 +0300

    IntroActivity added

--- Android3Todo/app/src/main/java/com/geektech/android3todo/IntroActivity.java
+++ Android3Todo/app/src/main/java/com/geektech/android3todo/IntroActivity.java
@@ -1,14 +1,14 @@
-package com.geektech.todoandroid3;
+package com.geektech.android3todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
